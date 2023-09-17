3f529ee4caf2e40e369abd7ce0b52aba4afbea0f
diyooor <diyorsattarov7@gmail.com>
2023-09-17 13:53:56 -0500

    Added additional test fixtures that expose the mishandling of whitespace in lexer

--- tests/lexer/lexer_tests.cpp
+++ tests/lexer/lexer_tests.cpp
@@ -6,3 +6,27 @@ TEST(LexerTests, TokenizeSimpleCode) {
     std::vector<Token> tokens = lexer.tokenize();
     ASSERT_EQ(tokens.size(), 15);
 }
+
+TEST(LexerTests, TokenizeBasicTypes) {
+    Lexer lexer("int x = 42;");
+    std::vector<Token> tokens = lexer.tokenize();
+    std::cout << "Actual tokens:\n";
+    for (const Token& token : tokens) {
+        std::cout << "Type: " << static_cast<int>(token.type) << ", Lexeme: " << token.lexeme << "\n";
+    }
+    ASSERT_EQ(tokens.size(), 9);
+    ASSERT_EQ(tokens[0].type, TokenType::INTEGER);
+    ASSERT_EQ(tokens[1].type, TokenType::IDENTIFIER);
+    //ASSERT_EQ(tokens[2].type, TokenType::EQUAL);
+    ASSERT_EQ(tokens[3].type, TokenType::INTEGER);
+}
+
+TEST(LexerTests, TokenizeWithWhitespace) {
+    Lexer lexer("  int  x   =  42  ;  ");
+    std::vector<Token> tokens = lexer.tokenize();
+    ASSERT_EQ(tokens.size(), 4);
+    // Ensure no whitespace tokens are generated
+    for (const auto& token : tokens) {
+        ASSERT_NE(token.type, TokenType::WHITESPACE);
+    }
+}
