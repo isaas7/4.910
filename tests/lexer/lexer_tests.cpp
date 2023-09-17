30c214b893c5d6fb711f7d6fe200424e6607a8a8
diyooor <diyorsattarov7@gmail.com>
2023-09-17 14:06:49 -0500

    Added TokenizeInvalidInput (fails)

--- tests/lexer/lexer_tests.cpp
+++ tests/lexer/lexer_tests.cpp
@@ -30,3 +30,10 @@ TEST(LexerTests, TokenizeWithWhitespace) {
         ASSERT_NE(token.type, TokenType::WHITESPACE);
     }
 }
+
+TEST(LexerTests, TokenizeInvalidInput) {
+    Lexer lexer("@$%&");
+    std::vector<Token> tokens = lexer.tokenize();
+    // Ensure that no valid tokens are generated for invalid input
+    ASSERT_EQ(tokens.size(), 0);
+}
