75d4342dfd831109819bf36b7c31142382500eb8
diyooor <diyorsattarov7@gmail.com>
2023-09-17 22:57:19 -0500

    Implemented  methods scanString, scanCharacterLiteral, isComment and added additional token types to tokenize switch

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -20,8 +20,24 @@ std::vector<Token> Lexer::tokenize() {
 }
 
 Token Lexer::scanToken() {
-    // Skip leading whitespace
-    while (current < source.length() && (source[current] == ' ' || source[current] == '\t' || source[current] == '\n' || source[current] == '\r')) {
+    while (current < source.length() && (source[current] == ' ' || source[current] == '\t' || source[current] == '\n' || source[current] == '\r' || isComment())) {
+        if (source[current] == '/') {
+            if (current + 1 < source.length() && source[current + 1] == '/') {
+                while (current < source.length() && source[current] != '\n') {
+                    current++;
+                }
+            } else if (current + 1 < source.length() && source[current + 1] == '*') {
+                current += 2;
+                while (current < source.length() - 1 && !(source[current] == '*' && source[current + 1] == '/')) {
+                    current++;
+                }
+                if (current < source.length() - 1) {
+                    current += 2;
+                } else {
+                    throw std::runtime_error("Lexer Error: Unterminated multi-line comment");
+                }
+            }
+        }
         current++;
     }
 
@@ -32,6 +48,10 @@ Token Lexer::scanToken() {
     char c = source[current++];
 
     switch(c) {
+        case '\'':
+            return scanCharacterLiteral();
+        case '"':
+            return scanString();
         case '+':
             return Token(TokenType::PLUS, "+");
         case '-':
@@ -49,9 +69,16 @@ Token Lexer::scanToken() {
         case ')':
             return Token(TokenType::RIGHT_PAREN, ")");
         case '{':
-            return Token(TokenType::LEFT_BRACE, "{");  // Recognize left brace
+            return Token(TokenType::LEFT_BRACE, "{");
         case '}':
-            return Token(TokenType::RIGHT_BRACE, "}"); // Recognize right brace
+            return Token(TokenType::RIGHT_BRACE, "}");
+        case ':':
+            if (peek() == ':') {
+                current++;
+                return Token(TokenType::DOUBLE_COLON, "::");
+            } else {
+                throw std::runtime_error("Lexer Error: Unrecognized character '" + std::string(1, c) + "'");
+            }
         default:
             if(isalpha(c)) {
                 return scanIdentifier();
@@ -62,6 +89,10 @@ Token Lexer::scanToken() {
             }
     }
 
+}
+
+bool Lexer::isComment() {
+    return (source[current] == '/' && (current + 1 < source.length() && (source[current + 1] == '/' || source[current + 1] == '*')));
 }
 
 
@@ -79,6 +110,42 @@ Token Lexer::scanNumber() {
     }
 
     return Token(TokenType::INTEGER, source.substr(start, current - start));
+}
+
+Token Lexer::scanString() {
+    int stringStart = current - 1; // Remember the starting position of the string
+
+    while (peek() != '"' && !isAtEnd()) {
+        if (peek() == '\\') {
+            current++;
+            if (isAtEnd()) {
+                throw std::runtime_error("Lexer Error: Unterminated string literal");
+            }
+            current++;
+        } else {
+            current++;
+        }
+    }
+
+    if (isAtEnd()) {
+        throw std::runtime_error("Lexer Error: Unterminated string literal");
+    }
+
+    current++;
+    std::string lexeme = source.substr(stringStart, current - stringStart);
+    return Token(TokenType::STRING, lexeme);
+}
+
+Token Lexer::scanCharacterLiteral() {
+    char c = advance();
+    if (c == '\\') {
+        c = advance();
+    }
+    if (peek() != '\'') {
+        throw std::runtime_error("Lexer Error: Invalid character literal");
+    }
+    advance();
+    return Token(TokenType::CHARACTER, std::string(1, c));
 }
 
 char Lexer::advance() {
