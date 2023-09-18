d100e1bcc20c41fc9dd89be85f0aa058b400ecd3
diyooor <diyorsattarov7@gmail.com>
2023-09-18 02:50:28 -0500

    Implement AST (Abstract Syntax Tree) class methods
    This commit includes the implementation of methods for the ASTNode base class and its derived classes. It defines the behavior of these classes, including printing their contents and providing access to values and identifiers where applicable. This lays the groundwork for building and manipulating the AST in the compiler.

---
+++ src/parser/ast.cpp
@@ -0,0 +1,55 @@
+#include "ast.h"
+#include <iostream> // For debugging purposes
+
+ASTNode::ASTNode(NodeType type) : type(type) {}
+
+ASTNode::~ASTNode() {}
+
+NodeType ASTNode::getType() const {
+    return type;
+}
+
+VariableDeclarationNode::VariableDeclarationNode(const std::string& identifier, int value)
+    : ASTNode(NodeType::VariableDeclaration), identifier(identifier), value(value) {}
+
+void VariableDeclarationNode::print(int indent) const {
+    for (int i = 0; i < indent; i++) {
+        std::cout << "  "; // Adjust the indentation as needed
+    }
+    std::cout << "VariableDeclarationNode: " << identifier << " = " << value << std::endl;
+}
+
+int VariableDeclarationNode::getValue() const {
+    return value;
+}
+
+const std::string& VariableDeclarationNode::getIdentifier() const {
+    return identifier;
+}
+
+AssignmentNode::AssignmentNode(const std::string& identifier, ASTNode* expression)
+    : ASTNode(NodeType::Assignment), identifier(identifier), expression(expression) {}
+
+AssignmentNode::~AssignmentNode() {
+    delete expression;
+}
+
+void AssignmentNode::print(int indent) const {
+    for (int i = 0; i < indent; i++) {
+        std::cout << "  "; // Adjust the indentation as needed
+    }
+    std::cout << "AssignmentNode: " << identifier << " = " << std::endl;
+    expression->print(indent + 1);
+}
+
+ASTNode* AssignmentNode::getExpression() const {
+    return expression;
+}
+
+const std::string& AssignmentNode::getIdentifier() const {
+    return identifier;
+}
+
+
+
+// Implement additional derived classes as needed
