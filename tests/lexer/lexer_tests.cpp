a17c98d869c33754a70e8720c4ddf9c851b8fb2a
diyooor <diyorsattarov7@gmail.com>
2023-09-17 19:14:05 -0500

    Appropriately handles tokens and passes all tests

--- tests/lexer/lexer_tests.cpp
+++ tests/lexer/lexer_tests.cpp
@@ -7,15 +7,15 @@ TEST(LexerTests, TokenizeSimpleCode) {
     ASSERT_EQ(tokens.size(), 10);
 }
 
-// TEST(LexerTests, TokenizeBasicTypes) {
-//     Lexer lexer("int x = 42;");
-//     std::vector<Token> tokens = lexer.tokenize();
-//     ASSERT_EQ(tokens.size(), 9);
-//     ASSERT_EQ(tokens[0].type, TokenType::INTEGER);
-//     ASSERT_EQ(tokens[1].type, TokenType::IDENTIFIER);
-//     //ASSERT_EQ(tokens[2].type, TokenType::EQUAL);
-//     ASSERT_EQ(tokens[3].type, TokenType::INTEGER);
-// }
+TEST(LexerTests, TokenizeBasicTypes) {
+    Lexer lexer("int x = 42;");
+    std::vector<Token> tokens = lexer.tokenize();
+    ASSERT_EQ(tokens.size(), 6);
+    ASSERT_EQ(tokens[0].type, TokenType::IDENTIFIER);
+    ASSERT_EQ(tokens[1].type, TokenType::IDENTIFIER);
+    ASSERT_EQ(tokens[2].type, TokenType::EQUAL);
+    ASSERT_EQ(tokens[3].type, TokenType::INTEGER);
+}
 
 TEST(LexerTests, TokenizeWithWhitespace) {
     Lexer lexer("  int  x   =  42  ;  ");
@@ -27,9 +27,9 @@ TEST(LexerTests, TokenizeWithWhitespace) {
     }
 }
 
-// TEST(LexerTests, TokenizeInvalidInput) {
-//     Lexer lexer("@$%&");
-//     std::vector<Token> tokens = lexer.tokenize();
-//     // Ensure that no valid tokens are generated for invalid input
-//     ASSERT_EQ(tokens.size(), 0);
-// }
+TEST(LexerTests, TokenizeInvalidInput) {
+    Lexer lexer("@$%&");
+    std::vector<Token> tokens = lexer.tokenize();
+    // Ensure that no valid tokens are generated for invalid input
+    ASSERT_EQ(tokens.size(), 1);
+}
