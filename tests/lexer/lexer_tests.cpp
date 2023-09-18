b6fcd264c9a5d79113cb3903a9ac2f53870830a9
diyooor <diyorsattarov7@gmail.com>
2023-09-17 22:57:44 -0500

    Implemented and passed all new tests -- closes Issue #4

--- tests/lexer/lexer_tests.cpp
+++ tests/lexer/lexer_tests.cpp
@@ -21,7 +21,6 @@ TEST(LexerTests, TokenizeWithWhitespace) {
     Lexer lexer("  int  x   =  42  ;  ");
     std::vector<Token> tokens = lexer.tokenize();
     ASSERT_EQ(tokens.size(), 6);
-    // Ensure no whitespace tokens are generated
     for (const auto& token : tokens) {
         ASSERT_NE(token.type, TokenType::WHITESPACE);
     }
@@ -43,4 +42,67 @@ TEST(LexerTests, MultiplicativeOperators) {
 TEST(LexerTests, CppKeywords) {
     Lexer lexer("if (condition) { return 42; }");
     std::vector<Token> tokens = lexer.tokenize();
+    ASSERT_EQ(tokens.size(), 10);
+}
+
+TEST(LexerTests, String) {
+    Lexer lexer("std::string greeting = \"Hello World\";");
+    std::vector<Token> tokens = lexer.tokenize();
+    ASSERT_EQ(tokens.size(), 8);
+}
+
+TEST(LexerTests, CharLiterals) {
+    Lexer lexer("char ch = \'A\';");
+    std::vector<Token> tokens = lexer.tokenize();
+    ASSERT_EQ(tokens.size(), 6);
+}
+
+TEST(LexerTests, StringCharLiterals) {
+    Lexer lexer("std::string greeting = \"Hello World\"; char ch = \'A\';");
+    std::vector<Token> tokens = lexer.tokenize();
+    ASSERT_EQ(tokens.size(), 13);
+}
+
+TEST(LexerTests, IdentifierDigits) {
+    Lexer lexer("cpp var123 = x42 + a1b2c3;");
+    std::vector<Token> tokens = lexer.tokenize();
+    ASSERT_EQ(tokens.size(), 8);
+}
+
+TEST(LexerTests, TokenizeSpecialCharacters) {
+    EXPECT_THROW({
+        Lexer lexer("@variable $amount %value &name");
+        std::vector<Token> tokens = lexer.tokenize();
+    }, std::runtime_error);
+}
+
+TEST(LexerTests, EmptyString) {
+    Lexer lexer("");
+    std::vector<Token> tokens = lexer.tokenize();
+    ASSERT_EQ(tokens.size(), 1);
+}
+
+TEST(LexerTests, TokenizeNonASCII) {
+    EXPECT_THROW({
+        Lexer lexer("你好, 世界\nαβγδε");
+        std::vector<Token> tokens = lexer.tokenize();
+    }, std::runtime_error);
+}
+
+TEST(LexerTests, LineEndings) {
+    Lexer lexer("Line 1\r\nLine 2\nLine 3");
+    std::vector<Token> tokens = lexer.tokenize();
+    ASSERT_EQ(tokens.size(), 7);
+}
+
+TEST(LexerTests, BigNumber) {
+    Lexer lexer("int bigNumber = 12345678901234567890;");
+    std::vector<Token> tokens = lexer.tokenize();
+    ASSERT_EQ(tokens.size(), 6);
+}
+
+TEST(LexerTests, EscapedCharacters) {
+    Lexer lexer("std::string message = \"Hello\nWorld\";");
+    std::vector<Token> tokens = lexer.tokenize();
+    ASSERT_EQ(tokens.size(), 8);
 }
